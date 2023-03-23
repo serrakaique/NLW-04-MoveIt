f208a5d1a50eeee991fb0f4af3810b280c911c6b
Kaique Luiz Nascimento Serra <3SNLKaique@ache.com.br>
2023-03-23 13:06:38 -0300

    Projeto em react

---
+++ src/components/Countdown.tsx
@@ -0,0 +1,65 @@
+import { useContext } from 'react';
+import { CountdownContext } from '../contexts/CountdownContext';
+
+import styles from '../styles/components/Countdown.module.css';
+
+export function Countdown() {
+const {
+    minutes,
+    seconds,
+    hasFinished,
+    isActive,
+    startCountdown,
+    resetCountdown,
+} = useContext(CountdownContext)
+
+    
+//padStart -> Se a string não tiver dois elementos, ele vai preencher em 0
+//ex. 25  - '2' '5'     5  - '0' '5'
+    const [minuteLeft, minuteRight]  = String(minutes).padStart(2, '0').split('');
+    const [secondLeft, secondRight]  = String(seconds).padStart(2, '0').split('');
+
+
+
+    return (
+        <div>
+            <div className={styles.countdownContainer}>
+                <div>
+                    <span>{minuteLeft}</span>
+                    <span>{minuteRight}</span>
+                </div>
+                <span>:</span>
+                <div>
+                    <span>{secondLeft}</span>
+                    <span>{secondRight}</span>
+                </div>
+            </div>
+
+            { hasFinished ? (
+                <button 
+                disabled
+                className={styles.countdownButton}>
+                    Ciclo encerrado    
+                </button>
+            ) : (
+                <>
+             { isActive ? (
+                <button 
+                type='button'
+                onClick={resetCountdown}
+                className={`${styles.countdownButton} ${styles.countdownButtonActive}`}>
+                    Abandonar ciclo
+                </button>
+            ) : (
+                <button type='button'
+                onClick={startCountdown}
+               className={styles.countdownButton}>
+                   Iniciar um ciclo
+               </button>
+            ) }
+                </>
+            )}
+        </div>
+//Se o isActive tiver ativo vai aparecer Abanadonar ciclo, se não vai aparecer Iniciar um ciclo
+    );
+}
